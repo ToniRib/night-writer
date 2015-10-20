d484bef1d42c4aa216d9ee3e48d0c86e9cc2fd55
ToniRib <tonimarierib@gmail.com>
2015-10-20 14:52:12 -0600

    Categorize: Remove unused category methods

--- lib/categorize.rb
+++ lib/categorize.rb
@@ -14,17 +14,8 @@ module Categorize
   def space?(char)
     convert_to_byte(char) == 32
   end
-
-  def lower?(char)
-    (97..122).to_a.include?(convert_to_byte(char))
-  end
-
+  
   def switch?(char)
     convert_to_byte(char) == 36
-  end
-
-  def punctuation?(char)
-    # ! ' , - . ?
-    [33, 39, 44, 45, 46, 63].include?(convert_to_byte(char))
   end
 end
