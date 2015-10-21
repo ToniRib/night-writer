64e896baae7b8b04bbf5f54ca0628f1320626b81
ToniRib <tonimarierib@gmail.com>
2015-10-21 14:03:23 -0600

    Converter: Cleans up tests

--- spec/converter_test.rb
+++ spec/converter_test.rb
@@ -28,70 +28,28 @@ class ConverterTest < Minitest::Test
     refute converter.capital?('a')
   end
 
-  def test_detects_a_space
-    converter = Converter.new
-    assert converter.space?(' ')
-  end
-
-  def test_rejects_a_non_space
-    converter = Converter.new
-    refute converter.space?('a')
-  end
-
-  def test_detects_a_dollar_sign
-    converter = Converter.new
-    assert converter.switch?('$')
-  end
-
-  def test_rejects_a_non_dollar_sign
-    converter = Converter.new
-    refute converter.switch?('*')
-  end
-
   def test_adds_a_shift_character_if_letter_is_capital
     converter = Converter.new
-    assert_equal '..00', converter.get_line(:top, 'N')
-    assert_equal '...0', converter.get_line(:middle, 'N')
-    assert_equal '.00.', converter.get_line(:bottom, 'N')
+    line_hash = { top: '..00', middle: '...0', bottom: '.00.' }
+    assert_equal line_hash, converter.get_all_lines('N')
   end
 
   def test_does_not_add_a_shift_character_if_letter_is_lowercase
     converter = Converter.new
-    assert_equal '00', converter.get_line(:top, 'n')
-    assert_equal '.0', converter.get_line(:middle, 'n')
-    assert_equal '0.', converter.get_line(:bottom, 'n')
+    line_hash = { top: '00', middle: '.0', bottom: '0.' }
+    assert_equal line_hash, converter.get_all_lines('n')
   end
 
-  def test_can_write_a_space
+  def test_can_convert_a_space_to_braille
     converter = Converter.new
-    assert_equal '..', converter.get_line(:top, ' ')
-    assert_equal '..', converter.get_line(:middle, ' ')
-    assert_equal '..', converter.get_line(:bottom, ' ')
+    line_hash = { top: '..', middle: '..', bottom: '..' }
+    assert_equal line_hash, converter.get_all_lines(' ')
   end
 
-  def test_can_write_an_exclamation_point
+  def test_can_convert_an_exclamation_point_to_braille
     converter = Converter.new
-    assert_equal '..', converter.get_line(:top, '!')
-    assert_equal '00', converter.get_line(:middle, '!')
-    assert_equal '0.', converter.get_line(:bottom, '!')
-  end
-
-  def test_can_get_all_three_lines_for_lowercase_letter
-    converter = Converter.new
-    braille_hash = { top: '00', middle: '.0', bottom: '0.' }
-    assert_equal braille_hash, converter.get_all_lines('n')
-  end
-
-  def test_can_get_all_three_lines_for_capital_letter
-    converter = Converter.new
-    braille_hash = { top: '..0.', middle: '...0', bottom: '.000' }
-    assert_equal braille_hash, converter.get_all_lines('Z')
-  end
-
-  def test_can_get_all_three_lines_for_space
-    converter = Converter.new
-    braille_hash = { top: '..', middle: '..', bottom: '..' }
-    assert_equal braille_hash, converter.get_all_lines(' ')
+    line_hash = { top: '..', middle: '00', bottom: '0.' }
+    assert_equal line_hash, converter.get_all_lines('!')
   end
 
   def test_can_get_all_three_lines_for_switch_dollar_sign
