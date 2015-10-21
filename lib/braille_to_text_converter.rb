042a71b5c927ebb18285fe108b9d3178ad621f79
ToniRib <tonimarierib@gmail.com>
2015-10-20 18:00:33 -0600

    NightReader: Finds character from braille

--- lib/braille_to_text_converter.rb
+++ lib/braille_to_text_converter.rb
@@ -1,25 +1,39 @@
 class BrailleToTextConverter
   TOP_LINE = { "abehkloruvz" => '0.',
-               "ijstw" => '.0',
+               "ijstw$" => '.0',
                "cdfgmnpqxy" => '00',
-               "!',-.?" => '..'
+               "!',-.? @" => '..'
   }.invert
 
-  MIDDLE_LINE = { "ackmux'-" => '..',
+  MIDDLE_LINE = { "ackmux'- @" => '..',
                   "bfilpsv,?" => '0.',
-                  "denoyz" => '.0',
+                  "denoyz$" => '.0',
                   "ghjqrtw!." => '00'
   }.invert
 
-  BOTTOM_LINE = { "abcdefghij," => '..',
+  BOTTOM_LINE = { "abcdefghij, " => '..',
                   "klmnopqrst!'" => '0.',
-                  "w." => '.0',
-                  "uvxyz-?" => '00'
+                  "w.@" => '.0',
+                  "uvxyz-?$" => '00'
   }.invert
 
-  SHIFT = [['..'], ['..'], ['.0']]
+  def get_character(braille_set)
+    matches = []
+    matches << find_char_match(TOP_LINE, braille_set[0].first)
+    matches << find_char_match(MIDDLE_LINE, braille_set[1].first)
+    matches << find_char_match(BOTTOM_LINE, braille_set[2].first)
+    find_common_character(matches)
+  end
 
-  SPACE = [['..'], ['..'], ['..']]
+  def find_common_character(matches)
+    top_chars = matches[0].chars
+    middle_chars = matches[1].chars
+    bottom_chars = matches[2].chars
+    (top_chars & middle_chars & bottom_chars).first
+  end
 
-  SWITCH = [['.0'], ['.0'], ['00']]
+  def find_char_match(line, set)
+    line.find { |k, v| k.include?(set) }.last
+  end
+
 end
--- spec/braille_to_text_converter_text.rb
+++ spec/braille_to_text_converter_text.rb
@@ -5,7 +5,27 @@ require './lib/braille_to_text_converter'
 require 'pry'
 
 class BrailleToTextConverterTest < Minitest::Test
-  def test_1
-    skip
+  def test_detects_a_character
+    converter = BrailleToTextConverter.new
+    a = [['0.'], ['..'], ['..']]
+    assert_equal 'a', converter.get_character(a)
+  end
+
+  def test_detects_a_different_character
+    converter = BrailleToTextConverter.new
+    z = [['0.'], ['.0'], ['00']]
+    assert_equal 'z', converter.get_character(z)
+  end
+
+  def test_detects_a_space
+    converter = BrailleToTextConverter.new
+    space = [['..'], ['..'], ['..']]
+    assert_equal ' ', converter.get_character(space)
+  end
+
+  def test_returns_common_character
+    converter = BrailleToTextConverter.new
+    matches = ['abcde', 'afghi', 'ajklm']
+    assert_equal 'a', converter.find_common_character(matches)
   end
 end
